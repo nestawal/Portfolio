3b4636bb34a2fad65a8b9b42bee1859aa0b95503
nestawal <nestawalgwe@gmail.com>
2025-01-07 03:48:23 +0300

    yo

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-
+import Wordgen from "./Wordgen"
 
 export default function Body (){
+   
+        let name = "Nesta Walgwe"
+        let specialties = ["software developer","IT technician/specialist"]
+        console.log(Wordgen(specialties))
+
+        let work = ""
+        for(let i = 0;i <specialties.length;i++ ){
+            work = specialties[i]
+        }
+        let showWork = Wordgen(work)
+        console.log(showWork)
+   
     return(
         <div className="container">
             <div className="item1">
-                <h1>Hello</h1>
-                <h3>I am Nesta Walgwe</h3>
-                <p> I am a:</p>
-                <ul>
-                    <li>Software Developer</li>
-                    <li>IT tecnician/specialist</li>
-                </ul>
+                <div>
+                    <h1>Hello</h1>
+                    <div>
+                    <h3>I am {Wordgen(name)}</h3>
+                    <h3> I am a : </h3>
+                    <h3>{specialties}</h3>
+                    </div>
+                </div>
             </div>
 
             <div className="item2">
@@ -22,8 +35,33 @@ export default function Body (){
                     <img src="/src/pics/github.png" alt="" className="socials"/>    
                 </div>
             </div>
-            <div className="item3">fool</div>
-            <div className="item4">fool</div>
+            <div className="item3">
+                <h1>Projects</h1>
+                <div>
+                <h3>Website template</h3>
+                <p>html.css.js</p>
+                </div>
+            </div>
+            <div className="item4">
+                <h1>Skills</h1>
+                <div>
+                    <h3>Frontend Development</h3>
+                    <h3>Design</h3>
+                    <h3>Figma</h3>
+                    <h3>Backend Development</h3>
+                    <h3>Database Management</h3>
+                    <h3>Python</h3>
+                    <h3>Django</h3>
+                    <h3>Javascript</h3>
+                    <h3>React</h3>
+                    <h3>Express</h3>
+                    <h3>Node</h3>
+                    <h3>Mongo</h3>
+                    <h3>sql</h3>
+                    <h3>kotlin</h3>
+                    <h3>Java</h3>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Wordgen.jsx
+++ src/Wordgen.jsx
@@ -2,25 +2,39 @@ import React ,{useState , useEffect} from "react";
 
  //function to generate words
    export default function Wordgen(a){
-    const [wordShow,setWordShow] = useState("")
-    
-    useEffect(()=>{
-        let currentWord =""
-        let i = 0
-        const gen = () => {
-            if(i<a.length){
-                currentWord += a[i]
-                setWordShow(currentWord)
-                i++
-                setTimeout(gen,200)
+   
+        const [wordShow,setWordShow] = useState("")
+
+        useEffect(()=>{
+            let currentWord = ""
+            let i = 0
+            const gen = () => {
+                if(i<a.length){
+                    currentWord += a[i]
+                    setWordShow(currentWord)
+                    i++
+                    setTimeout(gen,200)
+                }
             }
+    
+            gen();
+    
+            return  () => clearTimeout(gen)  
+                       
+            },[a]);
+            //end 
+            return wordShow 
+  
+   /*
+        let result = ""
+        for(let i=0;i<a.length;i++){
+            
+           result += `\n${a[i]}\n`;
         }
 
-        gen();
-
-        return  () => clearTimeout(gen)  
-                   
-        },[a]);
-        //end 
-        return wordShow 
+        return result
+       
+    }
+    */
+   
    }
